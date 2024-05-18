c5eb1a65b4777df5ad52c481d58c3fb4b5c04d6e
Bogdan <bogdan.pettrovic@gmail.com>
2024-05-18 04:57:59 +0200

    3.4 zavrsen

--- takmicenje2023 zadatak3/Program.cs
+++ takmicenje2023 zadatak3/Program.cs
@@ -2,8 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        int xp, yp, xz, yz, razdaljina1 = -1, min, xs = 0, ys = 0, razlikaX, razlikaY, xk = 0, yk = 0, kraj = 0, pomxz, pomyz, pomxp, pomyp;
-        int[] razdaljina = new int[4];
+        int xp, yp, xz, yz, razdaljina1 = -1, min, xs = 0, ys = 0, razlikaX, razlikaY, xk = 0, yk = 0, kraj = 0, pomxz, pomyz, pomxp, pomyp, brp = 0, brz = 0;
+        int[] razdaljina = new int[4], pasPoljaX = new int[20], pasPoljaY = new int[20], zecPoljaX = new int[20], zecPoljaY = new int[20];
         char[,] mat = new char[20, 20];
         Console.WriteLine("Unesite ulazne podatke");
         xp = int.Parse(Console.ReadLine());
@@ -62,6 +62,9 @@
             // pas
             for (int i = 0; i < 3; i++)
             {
+                pasPoljaX[brp] = xp;
+                pasPoljaY[brp] = yp;
+                brp++;
                 razlikaX = xp - xz;
                 razlikaY = yp - yz;
                 if (razlikaX == 0) xk = 0;
@@ -75,6 +78,9 @@
                 if (yk == 0 && xk == 0) kraj = 1;
             }
             //zec
+            zecPoljaX[brz] = xz;
+            zecPoljaY[brz] = yz;
+            brz++;
             xz += xs;
             yz += ys;
             if (xz < 0 || yz < 0 || xz > 19 || yz > 19) kraj = 2;
@@ -88,6 +94,21 @@
         //3.4
         Console.WriteLine("\n\n************ Odgovor 4 *************\n\n");
         mat = pocetnaBasta();
+        xp = pomxp;
+        yp = pomyp;
+        xz = pomxz;
+        yz = pomyz;
+        for (int i = 0; i < brz; i++)
+        {
+            for (int j = 0; j < brp; j++)
+                if (pasPoljaX[j] == zecPoljaX[i] && pasPoljaY[j] == zecPoljaY[i]) mat[pasPoljaX[j], pasPoljaY[j]] = 'Y';
+            if (mat[zecPoljaX[i], zecPoljaY[i]] != 'Y')
+                mat[zecPoljaX[i], zecPoljaY[i]] = 'O';
+        }
+        for(int i = 0; i < brp; i++)
+            if (mat[pasPoljaX[i], pasPoljaY[i]] != 'Y')
+                mat[pasPoljaX[i], pasPoljaY[i]] = 'X';
+        iscrtajBastu(mat);
     }
 
     static void iscrtajBastu(char[,] mat)
